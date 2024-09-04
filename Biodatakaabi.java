fe34445c7cfaf86f203c33fc143441b2825a71bd
kaabiiii <kaabimrfuad1@gamil.com>
2024-09-04 11:48:05 +0700

    Biodata kaabi

---
+++ Biodatakaabi.java
@@ -0,0 +1,17 @@
+
+
+public class Biodatakaabi {
+    public static void main(String[] args) {
+                System.out.println("---------------------------------------------------------");
+                System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+                System.out.println("---------------------------------------------------------");
+                System.out.println("Nama \t\t: Ka Abi Muhammad Robit Fuad");
+                System.out.println("NIM \t\t: 244107020163");
+                System.out.println("Kelas\t\t: TI 1B");
+                System.out.println("No Absen\t: 13");
+                System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+                System.out.println("Alamat  \t: Joyogrand,Merjosari");
+                System.out.println("No HP\t\t: 085707045614");
+                System.out.println("Hobi\t\t: Bermain Gitar");
+    }
+}
